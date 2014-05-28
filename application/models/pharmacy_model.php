527139f1104e9b9ea13ca58e53c78b25cb7bc4e9
molhaMaleh <molhamaleh@gmail.com>
2014-05-28 09:30:13 +0300

    replaced doctor id and patient id with their real name

--- application/models/pharmacy_model.php
+++ application/models/pharmacy_model.php
@@ -14,16 +14,25 @@ class Pharmacy_model extends CI_Model {
 			$data = $q->result();		
 			return $data[0]->id;		
 		}			
+	}	
+	public function deleteOrder($value)
+	{		
+		$this->db->where('id',$value);
+		$this->db->delete('order_medicine');					
 	}
-	
 	public function addOrder($data)
 	{
 		return $this->db->insert('order_medicine',$data);
 	}
 	
-	public function getOrders($limit,$start){
-		$this->db->limit($limit, $start);
-		if($q = $this->db->get('order_medicine')){
+	public function getOrders($limit,$start){		
+		$this->db->select('order_medicine.id,medicines.tradeName,medicines.caliber,medicines.quantity,doctors.firstName,doctors.lastName,patients.firstName pfirstName,patients.lastName plastName');   		
+		$this->db->from('order_medicine');
+		$this->db->join('medicines', 'order_medicine.medicineID = medicines.id','inner');		
+		$this->db->join('doctors', 'order_medicine.doctorID = doctors.id','inner');
+		$this->db->join('patients', 'order_medicine.patientID = patients.id','inner');
+		$this->db->limit($limit, $start);						
+		if($q = $this->db->get()){		
 			$data = $q->result();
 			return $data;
 		}
