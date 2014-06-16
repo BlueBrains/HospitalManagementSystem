2721a6dc5e85f6c4b4c311618d17e4fc763e6b16
molhaMaleh <molhamaleh@gmail.com>
2014-06-16 19:58:37 +0300

    doctor_view

--- application/views/doctor_view.php
+++ application/views/doctor_view.php
@@ -1,5 +1,29 @@
-<?php 
+<div class="panel panel-primary" style="margin-top:50px">
+  <div class="panel-heading">
+    <h3 class="panel-title">Profile</h3>
+  </div>
+  <div class="panel-body">
+  	
+<div class="media" style="margin-top:10px">
+  <div class="pull-left" style="margin: 15px 40px 10px 30px">
 
-
-echo $this->session->userdata('isDoctorLoggedIn')."</br>";
-echo $this->session->userdata('ID');
+    <img class="media-object img-responsive img-thumbnail" src="<?php echo base_url();?>photos/doctors/<?php echo $this->session->userdata('ID') ; ?>.jpg" alt="profile picture">
+  </div>
+  <div class="media-body" style="margin-top: 130px">
+    <h3 class="media-heading">Personal Informations:</h4>
+    <div style="margin-left: 40px">
+	    <h5><b>FirstName:</b></h5>
+	    <h5><b>lastName:</b></h5>
+	    <h5><b>Department:</b></h5>
+	</div> 
+  </div>
+</div>
+  </div>
+</div>
+<div class="panel panel-danger" style="margin-top:50px">
+  <div class="panel-heading">
+    <h3 class="panel-title">Notifications</h3>
+  </div>
+  <div class="panel-body">
+  </div>
+</div>
