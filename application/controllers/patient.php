c56bbbf325cc2dfaa202def6100b0c7bba9a6644
Eyad Arnabeh <eyadof@gmail.com>
2014-07-04 19:17:30 +0300

    finishing patient controller

--- application/controllers/patient.php
+++ application/controllers/patient.php
@@ -25,41 +25,17 @@ class patient extends REST_Controller{
 	}
 
 	function new_post(){
-		var_dump($this->input->post());
-		/*$this->load->model('patient_model');
+		$this->load->model('patient_model');
 		
-		$patient = array(array(
-			'fname' => $this->post('fname')),
-			'lname' => $this->post('lname')),
-			'avatar' => $this->post('avatar')),
-			'date_of_birth' => $this->post('date_of_birth')),
-			'n_town' => $this->post('n_town')),
-			'n_countery' => $this->post('n_countery')),
-			'person_id' => $this->post('person_id')),
-			'recored_number' => $this->post('recored_number')),
-			'language' => $this->post('language')),
-			'marital_state' => $this->post('marital_state')),
-			'comments' => $this->post('comments'))
-		);
+		$patient = array($this->post());
 		
-		$this->patient_model->create($patient);*/
+		$this->patient_model->create($patient);
 	}
 
 	function pr_post(){
 		$this->load->model('patient_model');
 		
-		$private = array(array(
-			'patient_id' => $this->post('patient_id'),
-			'address' => $this->post('address'),
-			'province' => $this->post('province'),
-			'c_countery' => $this->post('c_countery'),
-			'email' => $this->post('email'),
-			'phone' => $this->post('phone'),
-			'cell' => $this->post('cell'),
-			'position' => $this->post('position'),
-			'company' => $this->post('company'),
-			'comments' => $this->post('comments')
-		));
+		$private = array($this->post());
 		
 		$this->patient_model->insert_private_info($private);
 	}
@@ -67,11 +43,7 @@ class patient extends REST_Controller{
 	function fm_post(){
 		$this->load->model('patient_model');
 		
-		$family = array(array(
-			'patient_id' => $this->post('patient_id'),
-			's_patient_id' => $this->post('s_patient_id'),
-			'relationship' => $this->post('relationship')
-		));
+		$family = array($this->post());
 		
 		$this->patient_model->insert_family_info($family);
 	}
@@ -79,22 +51,60 @@ class patient extends REST_Controller{
 	function hl_post(){
 		$this->load->model('patient_model');
 		
-		$health = array(array(
-			'patient_id' => $this->post('patient_id'),
-			'recored_name' => $this->post('recored_name'),
-			'status' => $this->post('status'),
-			'category' => $this->post('category')
-		));
+		$health = array($this->post());
 		
 		$this->patient_model->insert_health_info($health);
 	}
 
 	function edit_get(){
-
 		$this->load->view('patient/edit.html');
 	}
 
 	function edit_put(){
-		echo "hi";
+		$id = $this->put('id');
+		$patient = array();
+		foreach ($this->put() as $key => $value) {
+			if($key != '_method' || $key != 'id' )
+				$patient[$key] = $value;
+		}
+
+		$this->load->model('patient_model');
+		$this->patient_model->update($id,$patient);
+	}
+
+	function pr_put(){
+		$id = $this->put('id');
+		$private = array();
+		foreach ($this->put() as $key => $value) {
+			if($key != '_method' || $key != 'id' )
+				$private[$key] = $value;
+		}
+
+		$this->load->model('patient_model');
+		$this->patient_model->update_private_info($id,$private);
+	}
+
+	function fm_put(){
+		$id = $this->put('id');
+		$family = array();
+		foreach ($this->put() as $key => $value) {
+			if($key != '_method' || $key != 'id' )
+				$family[$key] = $value;
+		}
+
+		$this->load->model('patient_model');
+		$this->patient_model->update_family_info($id,$family);
+	}
+
+	function hl_put(){
+		$id = $this->put('id');
+		$health = array();
+		foreach ($this->put() as $key => $value) {
+			if($key != '_method' || $key != 'id' )
+				$health[$key] = $value;
+		}
+
+		$this->load->model('patient_model');
+		$this->patient_model->update_health_info($id,$health);
 	}
 }
