0daeabb169c19ccece8251803fe1c4a377d3ee35
Eyad Arnabeh <eyadof@gmail.com>
2014-07-06 19:42:37 +0300

    dynamic title

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -3,7 +3,6 @@
 <head>
       <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Free Bootstrap Admin Template : Binary Admin</title>
 	<!-- BOOTSTRAP STYLES-->
     <link href="<?php echo base_url();?>assets/css/bootstrap.css" rel="stylesheet" />
     <!-- TABLE STYLES-->
@@ -18,7 +17,7 @@
     <link href="<?php echo base_url();?>assets/css/custom.css" rel="stylesheet" />
      <!-- GOOGLE FONTS-->
    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />       
-
+   <title><?php echo $title ?></title>
 </head>
 <body>
     <div id="wrapper">
