ae8ba0929a9d33352fe7942e3b71fabb03f731a7
Luay <red.falcon.luay@gmail.com>
2014-07-07 05:12:46 +0300

    fixed problem

--- application/controllers/radiograph_supervisor.php
+++ application/controllers/radiograph_supervisor.php
@@ -6,7 +6,7 @@ class radiograph_supervisor extends REST_Controller{
 	{
 		parent::__construct();
 		$this->load->model('radiograph_model');
-		// if ($this->session->userdata('tag') != "radiograph_supervisor")
+		// if (!isset($this->session->userdata('tag') || $this->session->userdata('tag') != "radiograph_supervisor")
 		// {
 			// echo "You dont have permission to start this action";
 		// }
@@ -19,80 +19,221 @@ class radiograph_supervisor extends REST_Controller{
 		
 	//		$data['nam']=$info->firstName." ".$info->lastName;
 			//$data['main_content'] = 'radiograph/radiograph_index';
+			$bar[0]=" fa-desktop ,ALL Requests,radiograph_supervisor/total_order_list,False";
+			$bar[1]=" fa-qrcode ,Un Seen Request,radiograph_supervisor/un_seen,False";
+			$bar[2]=" fa-qrcode ,Un Finished Request,radiograph_supervisor/order_list,False";
+			$bar[3]=" fa-qrcode ,Out Order Requests,radiograph_supervisor/radiograph_external_request_done,False";
+			$bar[4]=" fa-table ,Implemented Request,radiograph_supervisor/order_list_implemented,False";
+			$bar[5]=" fa-edit ,Out Order Manage,radiograph_supervisor/radiograph_external_request,False";
+			
 			$data['main_content'] = 'radiograph/test';	
-			$data['title'] = 'Welcome Admin';	
-			$data['bar1'] = "not specified";
-			$data['linkbar1'] ="#";
+			$data['section'] = 'radiograph';	
+			$data['side'] = $bar;
 			$data['idd']="2";
-			//$this->load->view('includes/template',$data);
-			$this->load->view('radiograph/test',$data);
+			$this->load->view('includes/template',$data);
+			//$this->load->view('radiograph/test',$data);
 		
 	}
 	
 	function total_order_list_get ()
 	{
 		$data['record'] = $this->radiograph_model->fetch_req(0);
-		    $data['main_content'] = 'radiograph/test';	
-			$data['title'] = 'Welcome Admin';	
-			$data['bar1'] = "not specified";
-			$data['linkbar1'] ="#";
-			$data['idd']="2";
-			
-		$this->load->view('radiograph/radiograph_order_list_view',$data);
-		
+		    $bar[0]=" fa-desktop ,ALL Requests,radiograph_supervisor/total_order_list,True";
+			$bar[1]=" fa-qrcode ,Un Seen Request,radiograph_supervisor/un_seen,False";
+			$bar[2]=" fa-qrcode ,Un Finished Request,radiograph_supervisor/order_list,False";
+			$bar[3]=" fa-qrcode ,Out Order Requests,radiograph_supervisor/radiograph_external_request_done,False";
+			$bar[4]=" fa-table ,Implemented Request,radiograph_supervisor/order_list_implemented,False";
+			$bar[5]=" fa-edit ,Out Order Manage,radiograph_supervisor/radiograph_external_request,False";
+			$data['side'] = $bar;
+			$data['main_content'] = 'radiograph/radiograph_order_list_view';	
+			$data['section'] = 'radiograph';	
+			$this->load->view('includes/template',$data);
 	}
 	
 	function order_list_implemented_get()
 	{
 		$data['record'] = $this->radiograph_model->fetch_req(-1);
-		$this->load->view('radiograph/radiograph_order_list_view',$data);
-		
+		    $bar[0]=" fa-desktop ,ALL Requests,radiograph_supervisor/total_order_list,False";
+			$bar[1]=" fa-qrcode ,Un Seen Request,radiograph_supervisor/un_seen,False";
+			$bar[2]=" fa-qrcode ,Un Finished Request,radiograph_supervisor/order_list,False";
+			$bar[3]=" fa-qrcode ,Out Order Requests,radiograph_supervisor/radiograph_external_request_done,False";
+			$bar[4]=" fa-table ,Implemented Request,radiograph_supervisor/order_list_implemented,True";
+			$bar[5]=" fa-edit ,Out Order Manage,radiograph_supervisor/radiograph_external_request,False";
+			
+			$data['main_content'] = 'radiograph/radiograph_order_list_view';	
+			$data['section'] = 'radiograph';
+			$data['side'] = $bar;	
+			$this->load->view('includes/template',$data);
 	}
 	
 	function order_list_get()
 	{
 		$data['record'] = $this->radiograph_model->fetch_req(-2);
-		$this->load->view('radiograph/radiograph_order_list_view',$data);
+				    $bar[0]=" fa-desktop ,ALL Requests,radiograph_supervisor/total_order_list,False";
+			$bar[1]=" fa-qrcode ,Un Seen Request,radiograph_supervisor/un_seen,False";
+			$bar[2]=" fa-qrcode ,Un Finished Request,radiograph_supervisor/order_list,True";
+			$bar[3]=" fa-qrcode ,Out Order Requests,radiograph_supervisor/radiograph_external_request_done,False";
+			$bar[4]=" fa-table ,Implemented Request,radiograph_supervisor/order_list_implemented,False";
+			$bar[5]=" fa-edit ,Out Order Manage,radiograph_supervisor/radiograph_external_request,False";
+			$data['side'] = $bar;
+			$data['main_content'] = 'radiograph/radiograph_order_list_view';	
+			$data['section'] = 'radiograph';	
+			$this->load->view('includes/template',$data);
 		
 	}
 	
 	function radiograph_external_request_done_get ()
 	{
 		$data['record'] = $this->radiograph_model->fetch_req(-3);
-		$this->load->view('radiograph/radiograph_order_list_view',$data);
+			$bar[0]=" fa-desktop ,ALL Requests,radiograph_supervisor/total_order_list,False";
+			$bar[1]=" fa-qrcode ,Un Seen Request,radiograph_supervisor/un_seen,False";
+			$bar[2]=" fa-qrcode ,Un Finished Request,radiograph_supervisor/order_list,False";
+			$bar[3]=" fa-qrcode ,Out Order Requests,radiograph_supervisor/radiograph_external_request_done,True";
+			$bar[4]=" fa-table ,Implemented Request,radiograph_supervisor/order_list_implemented,False";
+			$bar[5]=" fa-edit ,Out Order Manage,radiograph_supervisor/radiograph_external_request,False";
+			$data['side'] = $bar;
+			$data['main_content'] = 'radiograph/radiograph_order_list_view';	
+			$data['section'] = 'radiograph';	
+			$this->load->view('includes/template',$data);
 		
 	}
 	 
 	function un_seen_get()
 	{
 		$data['record'] = $this->radiograph_model->fetch_req(-4);
-		$this->load->view('radiograph/radiograph_order_list_view',$data);
+			$bar[0]=" fa-desktop ,ALL Requests,radiograph_supervisor/total_order_list,False";
+			$bar[1]=" fa-qrcode ,Un Seen Request,radiograph_supervisor/un_seen,True";
+			$bar[2]=" fa-qrcode ,Un Finished Request,radiograph_supervisor/order_list,False";
+			$bar[3]=" fa-qrcode ,Out Order Requests,radiograph_supervisor/radiograph_external_request_done,False";
+			$bar[4]=" fa-table ,Implemented Request,radiograph_supervisor/order_list_implemented,False";
+			$bar[5]=" fa-edit ,Out Order Manage,radiograph_supervisor/radiograph_external_request,False";
+			$data['side'] = $bar;
+			$data['main_content'] = 'radiograph/radiograph_order_list_view';	
+			$data['section'] = 'radiograph';	
+			$this->load->view('includes/template',$data);
 	} 	
 	
 	function radiograph_external_request_get()
 	{
-		$this->load->view('radiograph/radiograph_external_request_view');
+			
+			$bar[0]=" fa-desktop ,ALL Requests,radiograph_supervisor/total_order_list,False";
+			$bar[1]=" fa-qrcode ,Un Seen Request,radiograph_supervisor/un_seen,False";
+			$bar[2]=" fa-qrcode ,Un Finished Request,radiograph_supervisor/order_list,False";
+			$bar[3]=" fa-qrcode ,Out Order Requests,radiograph_supervisor/radiograph_external_request_done,False";
+			$bar[4]=" fa-table ,Implemented Request,radiograph_supervisor/order_list_implemented,False";
+			$bar[5]=" fa-edit ,Out Order Manage,radiograph_supervisor/radiograph_external_request,True";
+			$data['side'] = $bar;
+			$data['main_content'] = 'radiograph/radiograph_external_request_view';	
+			$data['section'] = 'radiograph';	
+			$this->load->view('includes/template',$data);	
 	}
 	
 	function radiograph_external_request_sign_get()
 	{
 		
 		$data['record'] = $this->radiograph_model->create_req(-1);	
-		$this->load->view('radiograph/test');
+		
+			$bar[0]=" fa-desktop ,ALL Requests,radiograph_supervisor/total_order_list,True";
+			$bar[1]=" fa-qrcode ,Un Seen Request,radiograph_supervisor/un_seen,True";
+			$bar[2]=" fa-qrcode ,Un Finished Request,radiograph_supervisor/order_list,False";
+			$bar[3]=" fa-qrcode ,Out Order Requests,radiograph_supervisor/radiograph_external_request_done,False";
+			$bar[4]=" fa-table ,Implemented Request,radiograph_supervisor/order_list_implemented,False";
+			$bar[5]=" fa-edit ,Out Order Manage,radiograph_supervisor/radiograph_external_request,False";
+			$data['side'] = $bar;			
+			$data['main_content'] = 'radiograph/radiograph_order_list_view';	
+			$data['section'] = 'radiograph';	
+			$this->load->view('includes/template',$data);	
+		
 	}
 	
-	function confirm_request_get($id)
-	{
+	function u_get(){
+		$request = $this->radiograph_model->fetch_req($this->get('id'));
+		if($this->response->format == 'html'){
+			$data['request'] = $request;
+			$data['main_content']='radiograph/test';
 			
-		$this->load->view('radiograph/upload_result',$data);
+			$this->load->view('includes/template',$data);
+		}
+		else 
+			$this->response($request,200);
+	}
+	
+	function confirm_request_get()
+	{
+		$this->radiograph_model->confirm($this->get('id'));	
+		
+		$this->total_order_list_get();	
+	
 	}	
 	
-	function finish_request_get($id)
+
+	function finish_request_get()
 	{
-		$this->load->view('radiograph/test',$data);
-		$data['record'] = $this->radiograph_model->create_req(-1);
+		$data['req_id']=$this->get('id');
+
+			$bar[0]=" fa-desktop ,ALL Requests,radiograph_supervisor/total_order_list,True";
+			$bar[1]=" fa-qrcode ,Un Seen Request,radiograph_supervisor/un_seen,False";
+			$bar[2]=" fa-qrcode ,Un Finished Request,radiograph_supervisor/order_list,False";
+			$bar[3]=" fa-qrcode ,Out Order Requests,radiograph_supervisor/radiograph_external_request_done,False";
+			$bar[4]=" fa-table ,Implemented Request,radiograph_supervisor/order_list_implemented,False";
+			$bar[5]=" fa-edit ,Out Order Manage,radiograph_supervisor/radiograph_external_request,False";
+			$data['side'] = $bar;			
+			$data['main_content'] = 'radiograph/upload_form';	
+			$data['section'] = 'radiograph';	
+			$this->load->view('includes/template',$data);		
 	}
 	
+	function upload_image_post()
+	{
+		if ($this->radiograph_model->upload_image($this->post('id')))
+		{	
+			$bar[0]=" fa-desktop ,ALL Requests,radiograph_supervisor/total_order_list,True";
+			$bar[1]=" fa-qrcode ,Un Seen Request,radiograph_supervisor/un_seen,False";
+			$bar[2]=" fa-qrcode ,Un Finished Request,radiograph_supervisor/order_list,False";
+			$bar[3]=" fa-qrcode ,Out Order Requests,radiograph_supervisor/radiograph_external_request_done,False";
+			$bar[4]=" fa-table ,Implemented Request,radiograph_supervisor/order_list_implemented,False";
+			$bar[5]=" fa-edit ,Out Order Manage,radiograph_supervisor/radiograph_external_request,False";
+			$data['side'] = $bar;			
+			$data['main_content'] = 'radiograph/radiograph_order_list_view';	
+			$data['section'] = 'radiograph';	
+			$this->load->view('includes/template',$data);
+
+		}
+		else
+			{
+			$bar[0]=" fa-desktop ,ALL Requests,radiograph_supervisor/total_order_list,False";
+			$bar[1]=" fa-qrcode ,Un Seen Request,radiograph_supervisor/un_seen,False";
+			$bar[2]=" fa-qrcode ,Un Finished Request,radiograph_supervisor/order_list,False";
+			$bar[3]=" fa-qrcode ,Out Order Requests,radiograph_supervisor/radiograph_external_request_done,False";
+			$bar[4]=" fa-table ,Implemented Request,radiograph_supervisor/order_list_implemented,False";
+			$bar[5]=" fa-edit ,Out Order Manage,radiograph_supervisor/radiograph_external_request,False";
+			$data['side'] = $bar;			
+			$data['main_content'] = 'radiograph/upload_form';	
+			$data['section'] = 'radiograph';	
+		//	$this->load->view('includes/template',$data);	
+			}			
+	}
+	
+	function show_result_get()
+	{
+		$data['record']=$this->radiograph_model->show_image($this->get('id'));
+		if (isset($data))
+		{	
+			$bar[0]=" fa-desktop ,ALL Requests,radiograph_supervisor/total_order_list,False";
+			$bar[1]=" fa-qrcode ,Un Seen Request,radiograph_supervisor/un_seen,False";
+			$bar[2]=" fa-qrcode ,Un Finished Request,radiograph_supervisor/order_list,False";
+			$bar[3]=" fa-qrcode ,Out Order Requests,radiograph_supervisor/radiograph_external_request_done,False";
+			$bar[4]=" fa-table ,Implemented Request,radiograph_supervisor/order_list_implemented,False";
+			$bar[5]=" fa-edit ,Out Order Manage,radiograph_supervisor/radiograph_external_request,False";
+			$data['side'] = $bar;			
+			$data['main_content'] = 'radiograph/show_result';	
+			$data['section'] = 'radiograph';	
+			$this->load->view('includes/template',$data);
+
+		}
+		
+		
+	}
 	function delete($id)
 	{
 		
